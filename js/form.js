3862a180484485d5e1294188ca86f409434618e0
Maxim Voropai <muckam49@mail.ru>
2017-09-05 14:40:35 +0800

    to drag was implemented for filter and realized changes to filter

--- js/form.js
+++ js/form.js
@@ -1,19 +1,27 @@
 'use strict';
 
 (function () {
+  var FILTERS = {
+    chrome: 'grayscale',
+    sepia: 'sepia',
+    marvin: 'invert',
+    phobos: 'blur',
+    heat: 'brightness'
+  };
+
   var documentEscPressHandler = function (evt) {
     window.utils.isEscEvent(evt, hiddenUploadOverlay);
   };
 
   var showUploadOverlay = function () {
-    uploadOverlay.classList.remove('hidden');
+    window.utils.showElement(uploadOverlay);
 
     document.addEventListener('keydown', documentEscPressHandler);
     uploadFormCancel.addEventListener('keydown', uploadFormCancelEnterPressHandler);
   };
 
   var hiddenUploadOverlay = function () {
-    uploadOverlay.classList.add('hidden');
+    window.utils.hiddenElement(uploadOverlay);
 
     document.removeEventListener('keydown', documentEscPressHandler);
     uploadFormCancel.removeEventListener('keydown', uploadFormCancelEnterPressHandler);
@@ -120,11 +128,48 @@
     }
   };
 
+  var changeFilter = function (effect, value) {
+    var effectValue;
+
+    switch (effect) {
+      case 'phobos':
+        effectValue = FILTERS[effect] + '(' + (3 * value) + 'px)';
+        break;
+      case 'heat':
+        effectValue = FILTERS[effect] + '(' + (3 * value) + ')';
+        break;
+      case 'none':
+        effectValue = '';
+        break;
+
+      default:
+        effectValue = FILTERS[effect] + '(' + (1 * value) + ')';
+        break;
+    }
+
+    effectImagePreview.style.filter = effectValue;
+  };
+
   var applyEffectImagePreview = function (elem) {
+    currentRadio = elem.value;
+
     resetCurrentEffect();
 
-    currentEffect = 'effect-' + elem.value;
+    currentEffect = 'effect-' + currentRadio;
+    changeFilter(currentRadio, 1);
+
+    if (currentRadio === 'none') {
+      currentEffect = '';
+      window.utils.hiddenElement(uploadEffectLevel);
+      return;
+    }
+
+    window.utils.showElement(uploadEffectLevel);
     effectImagePreview.classList.add(currentEffect);
+
+    var maxScaleValue = uploadEffectLevelLine['offsetWidth'];
+    setPinPosition(uploadEffectLevelPin, maxScaleValue);
+    setScaleWidth(uploadEffectLevelVal, maxScaleValue, maxScaleValue);
   };
 
   var checkLimits = function (currentValue, minValue, maxValue) {
@@ -176,7 +221,7 @@
     }
   };
 
-  var uploadEffectKeydownHandler = function (evt) {
+  var uploadEffectKeyDownHandler = function (evt) {
     if (evt.keyCode === window.utils.ENTER_KEYCODE) {
       if (evt.target.tagName === 'LABEL') {
         var input = uploadEffect.querySelector('#' + evt.target.htmlFor);
@@ -196,8 +241,72 @@
   var effectImagePreview = uploadEffect.querySelector('.effect-image-preview');
 
   var currentEffect = '';
+  var currentRadio = 'none';
+
   uploadEffect.addEventListener('click', uploadEffectClickHandler);
-  uploadEffect.addEventListener('keydown', uploadEffectKeydownHandler);
+  uploadEffect.addEventListener('keydown', uploadEffectKeyDownHandler);
 
   var uploadResizeControlsValue = uploadEffect.querySelector('.upload-resize-controls-value');
+
+  var setPinPosition = function (elem, position) {
+    var minLocationPositionX = 0;
+    var maxLocationPositionX = uploadEffectLevelLine.offsetWidth;
+
+    elem.style.left = position + 'px';
+
+    if (elem.offsetLeft < minLocationPositionX) {
+      position = 0;
+    }
+
+    if (elem.offsetLeft > maxLocationPositionX) {
+      position = maxLocationPositionX;
+    }
+
+    elem.style.left = position + 'px';
+
+    return position;
+  };
+
+  var setScaleWidth = function (scale, value, maxValue) {
+    var scaleValue = value / maxValue;
+
+    scale.style.width = scaleValue * 100 + '%';
+
+    return scaleValue;
+  };
+
+  var uploadEffectLevel = uploadEffect.querySelector('.upload-effect-level');
+  var uploadEffectLevelLine = uploadEffectLevel.querySelector('.upload-effect-level-line');
+  var uploadEffectLevelVal = uploadEffectLevel.querySelector('.upload-effect-level-val');
+  var uploadEffectLevelPin = uploadEffectLevelLine.querySelector('.upload-effect-level-pin');
+  uploadEffectLevelPin.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCoordsX = evt.clientX;
+
+    var mouseMoveHandler = function (moveEvt) {
+      evt.preventDefault();
+
+      var shiftX = startCoordsX - moveEvt.clientX;
+
+      startCoordsX = moveEvt.clientX;
+
+      var pinPositionX = uploadEffectLevelPin.offsetLeft - shiftX;
+
+      var position = setPinPosition(uploadEffectLevelPin, pinPositionX);
+      var scaleValue = setScaleWidth(uploadEffectLevelVal, position, uploadEffectLevelLine.offsetWidth);
+
+      changeFilter(currentRadio, scaleValue);
+    };
+
+    var mouseUpHandler = function (upEvt) {
+      evt.preventDefault();
+
+      uploadEffect.removeEventListener('mousemove', mouseMoveHandler);
+      uploadEffect.removeEventListener('mouseup', mouseUpHandler);
+    };
+
+    uploadEffect.addEventListener('mousemove', mouseMoveHandler);
+    uploadEffect.addEventListener('mouseup', mouseUpHandler);
+  });
 })();
