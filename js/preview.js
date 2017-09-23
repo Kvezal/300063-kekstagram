fe841a20f35f982053cbe5a7c6fd36b561144432
Maxim Voropai <muckam49@mail.ru>
2017-09-23 16:38:44 +0800

    Update 2017-09-23T08:38:40.867Z

--- js/preview.js
+++ js/preview.js
@@ -32,6 +32,19 @@
   var closePreview = galleryOverlay.querySelector('.gallery-overlay-close');
   closePreview.addEventListener('click', closePreviewClickHandler);
 
+  var fileChooser = document.querySelector('#upload-file');
+  var previewImages = document.querySelector('.effect-image-preview');
+  var effectImages = document.querySelectorAll('.upload-effect-preview');
+
+  var addAvatarPhoto = function (reader) {
+    previewImages.src = reader.result;
+    Array.prototype.forEach.call(effectImages, function (item) {
+      item.style.backgroundImage = 'url(' + reader.result + ')';
+    });
+  };
+
+  window.uploadPhoto(fileChooser, addAvatarPhoto);
+
   window.preview = {
     showPhoto: function (elementNumber, photos) {
       showGalleryOverlay();
